fcffc21d9a77d634f8e212abd3f27209479473c2
Chris Lee <chl@umich.edu>
2016-03-08 21:30:10 -0500

    fix sound delays and add led lights

--- floorpiano.py
+++ floorpiano.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 
 import pygame.mixer
 
+import RPi.GPIO as GPIO
+import time
+
 # Modes
 piano = True
 guitar = False
@@ -26,52 +29,56 @@ def converter(char):
 		play_8()
 
 def play_1():
+	print ("LED 1 on")
+	GPIO.output(18,GPIO.HIGH)
 	if piano:
-		piano_c1.play()
+		piano_c1.play(maxtime=800)
 	elif guitar:
-		guitar_1.play()
+		guitar_1.play(maxtime=800)
 
 def play_2():
+	print ("LED 2 on")
+	GPIO.output(17,GPIO.HIGH)
 	if piano:
-		piano_d.play()
+		piano_d.play(maxtime=800)
 	elif guitar:
-		guitar_2.play()
+		guitar_2.play(maxtime=800)
 
 def play_3():
 	if piano:
-		piano_e.play()
+		piano_e.play(maxtime=800)
 	elif guitar:
-		guitar_3.play()
+		guitar_3.play(maxtime=800)
 
 def play_4():
 	if piano:
-		piano_f.play()
+		piano_f.play(maxtime=800)
 	elif guitar:
-		guitar_4.play()
+		guitar_4.play(maxtime=800)
 
 def play_5():
 	if piano:
-		piano_g.play()
+		piano_g.play(maxtime=800)
 	elif guitar:
-		guitar_5.play()
+		guitar_5.play(maxtime=800)
 
 def play_6():
 	if piano:
-		piano_a.play()
+		piano_a.play(maxtime=800)
 	elif guitar:
-		guitar_6.play()
+		guitar_6.play(maxtime=800)
 
 def play_7():
 	if piano:
-		piano_b.play()
+		piano_b.play(maxtime=800)
 	elif guitar:
-		guitar_7.play()
+		guitar_7.play(maxtime=800)
 
 def play_8():
 	if piano:
-		piano_c2.play()
+		piano_c2.play(maxtime=800)
 	elif guitar:
-		guitar_8.play()
+		guitar_8.play(maxtime=800)
 
 def pattern1():
 	print ("Starting pattern 1")
@@ -109,30 +116,45 @@ def pattern2():
 				elif event.key == K_ESCAPE:
 					print ("Exiting pattern1")
 					return
+
+
+def turnOffLEDLight(char):
+	if char == 'a':
+		print ("LED 1 off")
+		GPIO.output(18,GPIO.LOW)
+	elif char == 's':
+		print ("LED 2 off")
+		GPIO.output(17,GPIO.LOW)
 		
 
 if __name__=="__main__":
 	pygame.display.set_mode((120, 120), DOUBLEBUF | HWSURFACE)
+	pygame.mixer.pre_init(44100, -16, 2, 2048)
+	pygame.mixer.init(channels=32, buffer=512)
 	pygame.init()
-	pygame.mixer.init(channels=16, buffer=4096)
 
-	piano_c1 = pygame.mixer.Sound("piano/c1.wav")
-	piano_d = pygame.mixer.Sound("piano/d.wav")
-	piano_e = pygame.mixer.Sound("piano/e.wav")
-	piano_f = pygame.mixer.Sound("piano/f.wav")
-	piano_g = pygame.mixer.Sound("piano/g.wav")
-	piano_a = pygame.mixer.Sound("piano/a.wav")
-	piano_b = pygame.mixer.Sound("piano/b.wav")
-	piano_c2 = pygame.mixer.Sound("piano/c2.wav")
+	piano_c1 = pygame.mixer.Sound("/home/pi/Desktop/project/piano/c1.wav")
+	piano_d = pygame.mixer.Sound("/home/pi/Desktop/project/piano/d.wav")
+	piano_e = pygame.mixer.Sound("/home/pi/Desktop/project/piano/e.wav")
+	piano_f = pygame.mixer.Sound("/home/pi/Desktop/project/piano/f.wav")
+	piano_g = pygame.mixer.Sound("/home/pi/Desktop/project/piano/g.wav")
+	piano_a = pygame.mixer.Sound("/home/pi/Desktop/project/piano/a.wav")
+	piano_b = pygame.mixer.Sound("/home/pi/Desktop/project/piano/b.wav")
+	piano_c2 = pygame.mixer.Sound("/home/pi/Desktop/project/piano/c2.wav")
 
-	guitar_1 = pygame.mixer.Sound("guitar/c.wav")
-	guitar_2 = pygame.mixer.Sound("guitar/d.wav")
-	guitar_3 = pygame.mixer.Sound("guitar/e.wav")
-	guitar_4 = pygame.mixer.Sound("guitar/g.wav")
-	guitar_5 = pygame.mixer.Sound("guitar/a.wav")
-	guitar_6 = pygame.mixer.Sound("guitar/dmin.wav")
-	guitar_7 = pygame.mixer.Sound("guitar/emin.wav")
-	guitar_8 = pygame.mixer.Sound("guitar/gmin.wav")
+	guitar_1 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/c.wav")
+	guitar_2 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/d.wav")
+	guitar_3 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/e.wav")
+	guitar_4 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/g.wav")
+	guitar_5 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/a.wav")
+	guitar_6 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/dmin.wav")
+	guitar_7 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/emin.wav")
+	guitar_8 = pygame.mixer.Sound("/home/pi/Desktop/project/guitar/gmin.wav")
+
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setwarnings(False)
+	GPIO.setup(17,GPIO.OUT)
+	GPIO.setup(18,GPIO.OUT)
 
     
 	running = True
@@ -172,3 +194,7 @@ if __name__=="__main__":
 					pattern1()
 				elif event.key == pygame.K_x:
 					pattern2()
+			elif event.type == KEYUP:
+				letter = pygame.key.name(event.key)
+				turnOffLEDLight(letter)
+				
